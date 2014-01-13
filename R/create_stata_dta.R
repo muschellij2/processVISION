880017b6c9a4e7ae89c63d1966769f0193254662
muschellij2 <muschellij2@gmail.com>
2014-01-13 11:21:51 -0500

    Fixed Create_stata_dta

--- R/create_stata_dta.R
+++ R/create_stata_dta.R
@@ -78,7 +78,7 @@ create_stata_dta <- function(df.list,
     if (!trunc32) {
       stoppers = max(nc) > 32
       if (any(stoppers)) {
-        print(xdname, " had over 32 character variable names")
+        print(paste0(xdname, " had over 32 character variable names"))
         print(cn[stoppers])
         stop("can't convert with over 32")	
       }
